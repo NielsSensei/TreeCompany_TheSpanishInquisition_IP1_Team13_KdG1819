745e892888e5fb7baad119de92da119f4802b5ba
XanderVeldeman <xander.veldeman@student.kdg.be>
2019-04-01 16:04:57 +0200

    Added basic identity
    - Renamed UI-MVC to UIMVC
    - Added Identity
    - Added SQLite NuGet package

--- UI-MVC/Controllers/HomeController.cs
+++ UI-MVC/Controllers/HomeController.cs
@@ -4,9 +4,9 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using UI_MVC.Models;
+using UIMVC.Models;
 
-namespace UI_MVC.Controllers
+namespace UIMVC.Controllers
 {
     public class HomeController : Controller
     {
