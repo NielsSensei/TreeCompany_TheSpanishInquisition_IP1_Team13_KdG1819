3558ff8bc2b8f5a67fb7471aabfdbd08f2ebb171
Niels Van Zandbergen <niels.vanzandbergen@student.kdg.be>
2019-04-02 12:02:04 +0200

    Update Scripts
    Sacha heeft wat aan de create geknoeid dankzij mijn gezaag en ik heb aan de seed geknoeid.

--- DB-Scripts/IP1_TSI_SEED_SCRIPT.sql
+++ DB-Scripts/IP1_TSI_SEED_SCRIPT.sql
@@ -1,7 +1,6 @@
 /* Platform */
 INSERT INTO dbo.platforms(name, siteUrl) 
 VALUES('Stad Antwerpen', 'www.cityofideas.be')
-
 /* Users 
 0 = Anonymous
 1 = loggedin
@@ -51,8 +50,7 @@ INSERT INTO dbo.userdetails(userID,zipcode,banned,gender,active,birthDate)
 VALUES(4,'9120',0,0,1,'1997-09-08')
 
 INSERT INTO dbo.userdetails(userID,zipcode,banned,active,orgName,description)
-Values(5,'9120',0,1,'The Spanish Inquisition', 'Een groep programmeurs dat klaar is voor de
-wereld overnemen omdat niemand hen verwacht')
+Values(5,'9120',0,1,'The Spanish Inquisition', 'Een groep programmeurs die klaar is om de wereld over te nemen omdat niemand hen verwacht')
 
 INSERT INTO dbo.userdetails(userID,zipcode,banned,gender,active,birthDate)
 VALUES(6,'9120',0,0,1,'1997-09-08')
@@ -65,8 +63,7 @@ VALUES(8,'9120',0,0,1,'1997-09-08')
 
 /* Organisationevents */
 INSERT INTO dbo.organisationevents(userID, name, description, startDate, endDate)
-VALUES(5,'The Inquisition','Een conferentie over hoe we Antwerpen kunnen introduceren tot de
-Spaanse cultuur','2019-03-31','2019-04-1')
+VALUES(5,'The Inquisition','Een conferentie over hoe we Antwerpen kunnen introduceren tot de Spaanse cultuur','2019-03-31','2019-04-1')
 
 /* Projects 
 Status: Niet gestart/Phase naam/Afgesloten
@@ -80,28 +77,45 @@ likeVisbility
 6 = alle 3
 */
 INSERT INTO dbo.projects(currentPhaseID,userID,platformID,title,goal,status,visible,likeVisibility)
-VALUES(1,7,1,'GROENplaats','De Antwerpse Groenplaats terug groen maken','Niet gestart',1,0)
+VALUES(1,7,1,'GROENplaats','De Antwerpse Groenplaats terug groen maken','NIET GESTART',1,6)
 
-/* Phases 
-Veranderingen door Sacha nodig.
-*/
+/* Phases */
 INSERT INTO dbo.phases(projectID, description, startDate, endDate)
 VALUES(1,'Vergroenen van de Groenplaats','2019-03-10','2019-03-30')
 
 INSERT INTO dbo.phases(projectID, description, startDate, endDate)
 VALUES(1,'Gebruik van nieuwe groene ruimte','2019-04-01','2019-04-30')
 
-/* Modules
-Veranderingen door Sacha nodig.
-*/
+/* Modules */
+INSERT INTO dbo.modules(projectID, onGoing, tags, isQuestionnaire)
+VALUES(1,1,'#Questionnaire,#ForTheClimate,#OpinionsAreImportant',1)
 
-/* QuestionnaireQuestions
-Parent tables need updates atm, no inserts yet
+/* QuestionnaireQuestions 
+0 = open                                                                                                                                                       
+1 = single               
+2 = multi                                                                                                                                                       
+3 = drop                 
+4 = mail
+We still need required bit for the table, waiting for Sacha.
 */
+INSERT INTO dbo.questionnairequestions(moduleID, questionTEXT, qType, required)
+VALUES(1,'Waarom wil je de Groenplaats terug groen?',0,1)
 
-/* Answers
-Parent tables need updates atm, no inserts yet
-*/
+INSERT INTO dbo.questionnairequestions(moduleID, questionTEXT, qType, required)
+VALUES(1,'Welke aanpassing wil je het liefst?',1,1)
+
+INSERT INTO dbo.questionnairequestions(moduleID, questionTEXT, qType, required)
+VALUES(1,'Vind je dat er ook een bloementuin op de Groenplaats past?',2,1)
+
+INSERT INTO dbo.questionnairequestions(moduleID, questionTEXT, qType, required)
+VALUES(1,'Hoeveel m2 aan gras moet er aangelegd worden?',3,1)
+
+INSERT INTO dbo.questionnairequestions(moduleID, questionTEXT, qType, required)
+VALUES(1,'Gelieve u email adres achter te halen als u updates wilt over het project',4,0)
+
+/* Answers */
+INSERT INTO dbo.answers(qQuestionID, userID, answerText)
+VALUES(1,1,'Een grijs stadshart is deprimerend.')
 
 /* Options
 Parent tables need updates atm, no inserts yet
