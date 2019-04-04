a9f4c34ff73692d5ece67d7416578ecca3ecd471
Niels Van Zandbergen <niels.vanzandbergen@student.kdg.be>
2019-04-04 19:12:20 +0200

    Fixed Issue #22
    Lap, Kleir

--- Domain/UserInput/IOT_Device.cs
+++ Domain/UserInput/IOT_Device.cs
@@ -5,13 +5,9 @@ namespace Domain.UserInput
     public class IOT_Device
     {
         // Added by NG
-        // Modified by XV & NVZ
+        // Modified by XV & NVZ & EKT & DM
         public int Id { get; set; }
         public double LocationX { get; set; }
         public double LocationY { get; set; }
-
-        //TODO bespreek issue #22
-        public Vote Vote { get; set; }
-        public List<Interaction> Interactions { get; set; }
     }
 }
