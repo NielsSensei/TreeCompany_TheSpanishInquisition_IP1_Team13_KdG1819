078ec9cd79ea67539bc1043d2a7990e4c1af92a6
Edwin <edwinkaiyin.tam@student.kdg.be>
2019-04-05 09:57:44 +0200

    Resolve issue image

--- Domain/Common/Video.cs
+++ Domain/Common/Video.cs
@@ -3,5 +3,11 @@ namespace Domain.Common
     public class Video : Media
     {
         public int LengthSeconds { get; set; }
+        public VideoExtentions AcceptedExtentions { get; set; }
+    }
+
+    public enum VideoExtentions
+    {
+        MP4, WEBM
     }
 }
