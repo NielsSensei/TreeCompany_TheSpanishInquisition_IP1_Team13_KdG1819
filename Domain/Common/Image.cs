3a5cb160ceb61724fdc5e06b6c2e4e358d1f79e0
edwinkaiyintam <45334938+edwinkaiyintam@users.noreply.github.com>
2019-04-05 10:03:33 +0200

    Resolve issue image (#25)
    KONO ISSUE #20 DA

--- Domain/Common/Image.cs
+++ Domain/Common/Image.cs
@@ -2,6 +2,11 @@ namespace Domain.Common
 {
     public class Image : Media
     {
-        
+        public ImageExtensions AcceptedExtensions { get; set; }
+    }
+
+    public enum ImageExtensions
+    {
+        JPEG, PNG, GIF
     }
 }
--- Domain/Common/Media.cs
+++ Domain/Common/Media.cs
@@ -3,10 +3,6 @@ namespace Domain.Common
     public class Media
     {
         //Added by NG
-
-        
-
-        public string Extension { get; set; }
         public bool UserMedia { get; set; }
         public string FileName { get; set; }
         public string BlobPath { get; set; }
