d9ef01671cbda5673debd3d2bb7e0e9b8285f4ae
Xander Veldeman <xander.veldeman@student.kdg.be>
2019-04-05 15:54:41 +0200

    Applied UI requirements
    - Better support for NPM
    - Optimized Pages
    - CSS finally works properly
    - The
    - Spanish
    - Inquisition

--- UI-MVC/wwwroot/dist/index.entry.js
+++ UI-MVC/wwwroot/dist/index.entry.js
@@ -81,56 +81,21 @@
 /******/
 /******/
 /******/ 	// Load entry module and return exports
-/******/ 	return __webpack_require__(__webpack_require__.s = "./wwwroot/js/site.js");
+/******/ 	return __webpack_require__(__webpack_require__.s = "./wwwroot/js/index.js");
 /******/ })
 /************************************************************************/
 /******/ ({
 
-/***/ "./node_modules/bootstrap/dist/css/bootstrap.css":
-/*!*******************************************************!*\
-  !*** ./node_modules/bootstrap/dist/css/bootstrap.css ***!
-  \*******************************************************/
+/***/ "./wwwroot/js/index.js":
+/*!*****************************!*\
+  !*** ./wwwroot/js/index.js ***!
+  \*****************************/
 /*! no static exports found */
-/***/ (function(module, exports, __webpack_require__) {
+/***/ (function(module, exports) {
 
-// extracted by mini-css-extract-plugin
-
-/***/ }),
-
-/***/ "./wwwroot/css/site.css":
-/*!******************************!*\
-  !*** ./wwwroot/css/site.css ***!
-  \******************************/
-/*! no static exports found */
-/***/ (function(module, exports, __webpack_require__) {
-
-// extracted by mini-css-extract-plugin
-
-/***/ }),
-
-/***/ "./wwwroot/js/site.js":
-/*!****************************!*\
-  !*** ./wwwroot/js/site.js ***!
-  \****************************/
-/*! no exports provided */
-/***/ (function(module, __webpack_exports__, __webpack_require__) {
-
-"use strict";
-__webpack_require__.r(__webpack_exports__);
-/* harmony import */ var bootstrap_dist_css_bootstrap_css__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! bootstrap/dist/css/bootstrap.css */ "./node_modules/bootstrap/dist/css/bootstrap.css");
-/* harmony import */ var bootstrap_dist_css_bootstrap_css__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(bootstrap_dist_css_bootstrap_css__WEBPACK_IMPORTED_MODULE_0__);
-/* harmony import */ var _css_site_css__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ../css/site.css */ "./wwwroot/css/site.css");
-/* harmony import */ var _css_site_css__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(_css_site_css__WEBPACK_IMPORTED_MODULE_1__);
-﻿// CSS Dependencies: Bootstrap
-
-
-
-// Custom CSS imports
-
-
-console.log('The \'site\' bundle has been loaded!')
+﻿console.log('The Index bundle has been loaded')
 
 /***/ })
 
 /******/ });
-//# sourceMappingURL=site.entry.js.map
+//# sourceMappingURL=index.entry.js.map
