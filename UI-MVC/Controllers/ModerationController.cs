127f8c9c466c063b4863df1df9bd51d77afddb54
Niels Van Zandbergen <niels.vanzandbergen@student.kdg.be>
2019-04-23 15:21:43 +0200

    Temp commit, still doubting how it's done

--- UI-MVC/Controllers/ModerationController.cs
+++ UI-MVC/Controllers/ModerationController.cs
@@ -1,3 +1,4 @@
+using System.Linq;
 using BL;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
@@ -16,8 +17,15 @@ namespace UIMVC.Controllers
         //TODO: Voeg hier een ROLE toe zodat je niet via de link hier geraakt!
         [HttpGet]
         [Authorize]
-        public IActionResult CollectAllIdeas()
+        public IActionResult CollectAllIdeas(string filter)
         {
+            if(filter.Contains("admin"))
+            {
+                return View(_ideaMgr.GetIdeas().Where(i => i.ReviewByAdmin));
+            }else if (filter.Contains("report"))
+            {
+                return View(_ideaMgr.GetIdeas().Where(i => i.Reported && !i.ReviewByAdmin));
+            }
             return View(_ideaMgr.GetIdeas());
         } 
     }
