8b1a6d566e245de3737477f4a75fc7668109a55a
Niels Van Zandbergen <niels.vanzandbergen@student.kdg.be>
2019-04-23 10:41:06 +0200

    IP13-48 Finished

--- UI-MVC/Controllers/ModerationController.cs
+++ UI-MVC/Controllers/ModerationController.cs
@@ -1,9 +1,4 @@
-using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
 using BL;
-using Domain.UserInput;
 using Microsoft.AspNetCore.Mvc;
 
 namespace UIMVC.Controllers
