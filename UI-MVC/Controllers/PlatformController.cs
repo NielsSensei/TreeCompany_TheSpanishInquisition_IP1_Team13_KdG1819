e8ad3f1cee22050cd05d2310cf8376a84333c303
XanderVeldeman <xander.veldeman@student.kdg.be>
2019-04-24 16:49:39 +0200

    Added Platform page
    * Platform page has preview of the projects
    * Added a service for the projects
    * Added a method to BL/ProjectManager for reading all projects of a platform

---
+++ UI-MVC/Controllers/PlatformController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BL;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.CodeAnalysis;
+
+namespace UIMVC.Controllers
+{
+    
+    public class PlatformController : Controller
+    {
+        private PlatformManager _platformMgr;
+
+        public PlatformController()
+        {
+            _platformMgr = new PlatformManager();
+        }
+
+        [Route("platform/{id}")]
+        public IActionResult Index(int id)
+        {
+            Domain.Users.Platform platform = _platformMgr.GetPlatform(id);
+            if (platform == null)
+            {
+                return RedirectToAction("HandleErrorCode", "Errors", 404);
+            }
+            return View(platform);
+        }
+    }
+}
