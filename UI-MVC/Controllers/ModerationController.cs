597d15dc3748e2b726ca6270adc374fd7e1fb11e
XanderVeldeman <xander.veldeman@student.kdg.be>
2019-04-27 12:00:37 +0200

    UI platform (#51)
    * Commit before fetch
    
    * Added Platform page
    
    * Platform page has preview of the projects
    * Added a service for the projects
    * Added a method to BL/ProjectManager for reading all projects of a platform
    
    * Create platforms
    
    You can now create platforms
    * Adds name and URL
    * Added GetNextAvailableId() to BL/PlatformManager
    * Added the CreatePlatformModel
    Note: showmenu.js werkt enkel correct in VS IIS Express. Andere, gebruiken nogsteeds de oude versie van de file.
    
    * Basic change platforms
    
    * Added edit platform button
    * Fixed update in DAL/repos/PlatformRepository
    
    * A little icing on the cake
    
    * What if there were no projects ?
    * What if there were no platforms ?
    I added a message for these dark thoughts
    
    * Fixes requested by senpai
    
    * It now uses the database as ID picker

--- UI-MVC/Controllers/ModerationController.cs
+++ UI-MVC/Controllers/ModerationController.cs
@@ -4,18 +4,22 @@ using Domain.UserInput;
 using Domain.Users;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using UIMVC.Models;
 
 namespace UIMVC.Controllers
 {
     public class ModerationController : Controller
     {
+        private readonly PlatformManager _platformMgr;
         private readonly IdeationQuestionManager _ideaMgr;
         private readonly UserManager _usrMgr;
 
         public ModerationController()
         {
             _ideaMgr = new IdeationQuestionManager();
+            _platformMgr = new PlatformManager();
             _usrMgr = new UserManager();
+
         }
         
         //TODO: Voeg hier een ROLE toe zodat je niet via de link hier geraakt!
@@ -24,6 +28,34 @@ namespace UIMVC.Controllers
         public IActionResult CollectAllIdeas()
         {
             return View(_ideaMgr.GetIdeas());
+        } 
+
+        [HttpGet]
+        public IActionResult CreatePlatform()
+        {
+            ViewData["platforms"] = _platformMgr.ReadAllPlatforms();
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult CreatePlatform(CreatePlatformModel cpm)
+        {
+            if (cpm == null)
+            {
+                return BadRequest("Platform cannot be null");
+            }
+            Platform platform = new Platform()
+            {
+                // Id = _platformMgr.GetNextAvailableId(),
+                Name = cpm.Name,
+                Url = cpm.Url,
+                Owners = new List<User>(),
+                Users = new List<User>()
+            };
+            
+            var newPlatform = _platformMgr.MakePlatform(platform);
+            
+            return RedirectToAction("Index", "Platform", new {Id = newPlatform.Id} );
         }
 
         //TODO: Voeg hier een ROLE toe zodat je niet via de link hier geraakt!
@@ -101,6 +133,7 @@ namespace UIMVC.Controllers
             _ideaMgr.DeleteReport(report);
             
             return RedirectToAction(controllerName: "Moderation", actionName: "CollectAllIdeas");
+
         }
     }
 }
---
+++ UI-MVC/Controllers/PlatformController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BL;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.CodeAnalysis;
+
+namespace UIMVC.Controllers
+{
+    
+    public class PlatformController : Controller
+    {
+        private PlatformManager _platformMgr;
+
+        public PlatformController()
+        {
+            _platformMgr = new PlatformManager();
+        }
+
+        [Route("Platform/{id}")]
+        public IActionResult Index(int id)
+        {
+            Domain.Users.Platform platform = _platformMgr.GetPlatform(id);
+            if (platform == null)
+            {
+                return RedirectToAction("HandleErrorCode", "Errors", 404);
+            }
+            return View(platform);
+        }
+
+        #region Create
+
+        public IActionResult EditPlatform(int id)
+        {
+            Domain.Users.Platform platform = _platformMgr.GetPlatform(id);
+            if (platform == null)
+            {
+                return RedirectToAction("HandleErrorCode", "Errors", 404);
+            }
+            return View(platform);
+        }
+
+        [HttpPost]
+        public IActionResult EditPlatform(Domain.Users.Platform platform)
+        {
+            _platformMgr.EditPlatform(platform);
+            // TODO: make the redirect work
+            return RedirectToAction("Index", new {id = platform.Id});
+        }
+
+        #endregion
+    }
+}
