d4d5a5ce50d72634feab945531589f12aec58a3a
NielsSensei <niels.vanzandbergen@student.kdg.be>
2019-04-30 14:47:25 +0100

    Feature/admin/ideation add cq (#67)
    * Fixing shirty dev errors.
    
    * Need to call the menu still
    
    Refurbishing the controller a bit
    
    * Pre lunch commit
    
    * preemptive commit
    
    * FUCK YEAH I DID IT

--- UI-MVC/Controllers/ModerationController.cs
+++ UI-MVC/Controllers/ModerationController.cs
@@ -1,6 +1,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using BL;
+using Domain.Projects;
 using Domain.UserInput;
 using Domain.Users;
 using Microsoft.AspNetCore.Authorization;
@@ -13,33 +14,14 @@ namespace UIMVC.Controllers
     {
         private readonly PlatformManager _platformMgr;
         private readonly IdeationQuestionManager _ideaMgr;
-        private readonly UserManager _usrMgr;
 
         public ModerationController()
         {
             _ideaMgr = new IdeationQuestionManager();
             _platformMgr = new PlatformManager();
-            _usrMgr = new UserManager();
-
         }
         
-        //TODO: Voeg hier een ROLE toe zodat je niet via de link hier geraakt!
-        [HttpGet]
-        [Authorize]
-        public IActionResult CollectAllIdeas(string filter = "all")
-        {
-            List<Idea> ideas = new List<Idea>();
-            
-            switch (filter)
-            {
-                case "all": ideas = _ideaMgr.GetIdeas(); break; 
-                case "admin": ideas = _ideaMgr.GetIdeas().FindAll(i => i.ReviewByAdmin); break;
-                case "report": ideas = _ideaMgr.GetIdeas().FindAll(i => !i.ReviewByAdmin && i.Reported); break;
-            }
-
-            return View(ideas);
-        }
-        
+        #region Platform
         //TODO: Voeg hier een ROLE toe zodat je niet via de link hier geraakt!
         [HttpGet]
         [Authorize]
@@ -48,7 +30,7 @@ namespace UIMVC.Controllers
             ViewData["platforms"] = _platformMgr.ReadAllPlatforms();
             return View();
         }
-
+        
         //TODO: Voeg hier een ROLE toe zodat je niet via de link hier geraakt!
         [HttpPost]
         [Authorize]
@@ -70,7 +52,59 @@ namespace UIMVC.Controllers
             
             return RedirectToAction("Index", "Platform", new {Id = newPlatform.Id} );
         }
+        #endregion
+        
+        #region Ideation 
+        //TODO add rolecheck hero we need to be admin yeet *@
+        [Authorize]
+        [HttpGet]
+        public IActionResult AddCentralQuestion(int ideation)
+        {
+            ViewData["Ideation"] = ideation;
+            return View();
+        }
+        
+        //TODO add rolecheck hero we need to be admin yeet *@
+        [Authorize]
+        [HttpPost]
+        public IActionResult AddCentralQuestion(CreateIdeationQuestionModel ciqm, int ideation)
+        {
+            if (ciqm == null)
+            {
+                return BadRequest("IdeationQuestion can't be null");
+            }
+            
+            IdeationQuestion iq = new IdeationQuestion()
+            {
+               Description = ciqm.Description,
+               SiteURL = ciqm.SiteURL,
+               QuestionTitle = ciqm.QuestionTitle,
+               Ideation = new Ideation(){ Id = ideation }
+            };
+                       
+            _ideaMgr.MakeQuestion(iq, ideation);
+            
+            return RedirectToAction("CollectIdeation", "Platform", new {Id = ideation});
+        }
+        
+        #region Ideas
+        //TODO: Voeg hier een ROLE toe zodat je niet via de link hier geraakt!
+        [HttpGet]
+        [Authorize]
+        public IActionResult CollectAllIdeas(string filter = "all")
+        {
+            List<Idea> ideas = new List<Idea>();
+            
+            switch (filter)
+            {
+                case "all": ideas = _ideaMgr.GetIdeas(); break; 
+                case "admin": ideas = _ideaMgr.GetIdeas().FindAll(i => i.ReviewByAdmin); break;
+                case "report": ideas = _ideaMgr.GetIdeas().FindAll(i => !i.ReviewByAdmin && i.Reported); break;
+            }
 
+            return View(ideas);
+        }
+        
         //TODO: Voeg hier een ROLE toe zodat je niet via de link hier geraakt!
         [HttpGet]
         [Authorize]
@@ -86,7 +120,7 @@ namespace UIMVC.Controllers
 
             return RedirectToAction(controllerName: "Errors", actionName: "HandleErrorCode", routeValues: id);
         }
-
+        
         //TODO: Voeg hier een ROLE toe zodat je niet via de link hier geraakt!
         [HttpPost]
         [Authorize]
@@ -165,5 +199,17 @@ namespace UIMVC.Controllers
                 _ideaMgr.EditIdea(foundIdea);
             }
         }
+        #endregion
+        #endregion
+        
+        
+        
+        
+
+        
+
+        
+
+        
     }
 }
