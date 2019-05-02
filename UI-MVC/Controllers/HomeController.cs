097c6babf179af6206d624b20ee056e768364ccd
Xander Veldeman <xander.veldeman@student.kdg.be>
2019-05-02 16:51:30 +0200

    Added a footer
    - Hell, 't was about time.
    - Pimped the about page
    - Privacy page is still bland

--- UI-MVC/Controllers/HomeController.cs
+++ UI-MVC/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using UIMVC.Models;
 
