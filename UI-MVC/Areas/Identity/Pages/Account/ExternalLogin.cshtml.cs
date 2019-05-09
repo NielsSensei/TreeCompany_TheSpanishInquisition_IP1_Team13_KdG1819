563f4c2abed1e0f6c0d78442307fbc33cf2e0986
XanderVeldeman <xander.veldeman@student.kdg.be>
2019-05-09 23:00:21 +0200

    Roles/implementation (#109)
    * Basics for roles
    
    - Added RoleService
    - ModerationController Authorize tags have been filled
    - RoleService has been added
    - CollectAllUsers now shows a users roles
    
    * LoginProviders assign users when creating a new user
    
    - modified externalLogin
    
    * CollectProject and CollectIdeation HTML role checks
    
    - RoleService expects a ClaimsPrincipal (usually ´User´ in cshtml)
    - Moved some of the buttons around
    
    * testuser senõr SuperAdmin
    
    - his/her credentials can be found in appsettings.json
    
    * Commit for branch switch
    
    - still need to work on RoleService/IsSameRoleOrHigher
    
    * Smashing with the banhammer
    
    - properly checks the roles of the user in advance
    - Users get banned until DateTime.Max
    - Becky, lemme smash
    
    * Form error (pls help)
    
    * Found & Fixed Xander's problem
    
    * senpai notice me
    
    * Assigning roles
    
    - You can now assign roles using the collectallusers tab
    - This should be the final commit: next up dev merge
    
    * Roles my dudes!
    
    - roles should be up and functional
    
    * Fix
    
    - The remainder of the TODO
    - Added the verified check to an idea

--- UI-MVC/Areas/Identity/Pages/Account/ExternalLogin.cshtml.cs
+++ UI-MVC/Areas/Identity/Pages/Account/ExternalLogin.cshtml.cs
@@ -11,6 +11,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
 using UIMVC.Areas.Identity.Data;
+using UIMVC.Services;
 using UIMVCUser = Domain.Identity.UIMVCUser;
 
 namespace UIMVC.Areas.Identity.Pages.Account
@@ -21,15 +22,18 @@ namespace UIMVC.Areas.Identity.Pages.Account
         private readonly SignInManager<UIMVCUser> _signInManager;
         private readonly UserManager<UIMVCUser> _userManager;
         private readonly ILogger<ExternalLoginModel> _logger;
+        private readonly RoleService _roleService;
 
         public ExternalLoginModel(
             SignInManager<UIMVCUser> signInManager,
             UserManager<UIMVCUser> userManager,
-            ILogger<ExternalLoginModel> logger)
+            ILogger<ExternalLoginModel> logger,
+            RoleService roleService)
         {
             _signInManager = signInManager;
             _userManager = userManager;
             _logger = logger;
+            _roleService = roleService;
         }
 
         [BindProperty]
@@ -130,6 +134,10 @@ namespace UIMVC.Areas.Identity.Pages.Account
                     {
                         await _signInManager.SignInAsync(user, isPersistent: false);
                         _logger.LogInformation("User created an account using {Name} provider.", info.LoginProvider);
+                        
+                        var userFound = await _userManager.FindByEmailAsync(user.UserName);
+                        _roleService.AssignToRole(userFound, Domain.Users.Role.LOGGEDIN);
+                        
                         return LocalRedirect(returnUrl);
                     }
                 }
