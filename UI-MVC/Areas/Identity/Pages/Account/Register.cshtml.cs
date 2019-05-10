885a35b4011a5b8c7845f393604a0955210ed139
NielsSensei <niels.vanzandbergen@student.kdg.be>
2019-05-10 08:45:05 +0100

    Shomenu swooped (#108)
    * Shomenu swooped
    
    AddPlatform
    RemoveIdeation
    
    Ook volledig AddTags gerefractored zodat het ook ne modal gebruikt.
    
    * Revert "Merge branch 'dev' into css-swoop/showmenu"
    
    This reverts commit ac9c16885cd0a1c67626ac6f5475fcb376d83250, reversing
    changes made to d217c798ace435ac0d5ef8a528676d02506d05ca.

--- UI-MVC/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ UI-MVC/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -1,10 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-using System.Net;
 using System.Text.Encodings.Web;
 using System.Threading.Tasks;
-using Domain.Users;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.UI.Services;
@@ -12,7 +10,6 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
 using UIMVC.Areas.Identity.Data;
-using UIMVC.Services;
 using UIMVCUser = Domain.Identity.UIMVCUser;
 
 namespace UIMVC.Areas.Identity.Pages.Account
@@ -24,20 +21,17 @@ namespace UIMVC.Areas.Identity.Pages.Account
         private readonly UserManager<UIMVCUser> _userManager;
         private readonly ILogger<RegisterModel> _logger;
         private readonly IEmailSender _emailSender;
-        private readonly RoleService _roleService;
 
         public RegisterModel(
             UserManager<UIMVCUser> userManager,
             SignInManager<UIMVCUser> signInManager,
             ILogger<RegisterModel> logger,
-            IEmailSender emailSender,
-            RoleService roleService)
+            IEmailSender emailSender)
         {
             _userManager = userManager;
             _signInManager = signInManager;
             _logger = logger;
             _emailSender = emailSender;
-            _roleService = roleService;
         }
 
         [BindProperty]
@@ -89,7 +83,6 @@ namespace UIMVC.Areas.Identity.Pages.Account
                     DateOfBirth = Input.DateOfBirth
                 };
                 var result = await _userManager.CreateAsync(user, Input.Password);
-                
                 if (result.Succeeded)
                 {
                     _logger.LogInformation("User created a new account with password.");
@@ -103,10 +96,6 @@ namespace UIMVC.Areas.Identity.Pages.Account
 
                     await _emailSender.SendEmailAsync(Input.Email, "Confirm your email",
                         $"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.");
-                    
-                    
-                    var userFound = await _userManager.FindByEmailAsync(user.UserName);
-                    _roleService.AssignToRole(userFound, Role.LOGGEDIN);
 
                     // await _signInManager.SignInAsync(user, isPersistent: false);
                     return LocalRedirect(returnUrl);
