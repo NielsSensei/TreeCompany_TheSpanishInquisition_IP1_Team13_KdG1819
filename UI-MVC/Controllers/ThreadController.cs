0239b5149e83294263b6f6ca0530e7fedadd3a35
NielsSensei <niels.vanzandbergen@student.kdg.be>
2019-05-10 08:30:41 +0100

    Feature/logged in/flag idea (#107)
    * Pretesting the JS.
    
    * Did not account for invis projects.
    
    * Switching branch.
    
    * Flag is now with a modal.
    
    * Pre Testing
    
    Modal replaced my whole js work from yesterday.
    
    Report modal is gelijkaardig aan reactionmodal. Thanks @xanderveldeman  for introducing me to it.
    
    Maakt ook gebruik van statusmessages.
    
    * Done

--- UI-MVC/Controllers/ThreadController.cs
+++ UI-MVC/Controllers/ThreadController.cs
@@ -5,7 +5,6 @@ using Domain.Identity;
 using Domain.UserInput;
 using Domain.Users;
 using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using UIMVC.Services;
 
