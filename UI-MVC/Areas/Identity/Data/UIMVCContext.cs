c4d66460cc39bd200fb06be5d9fef9b2579e08dc
Niels Van Zandbergen <niels.vanzandbergen@student.kdg.be>
2019-05-14 19:18:11 +0200

    aaa

--- UI-MVC/Areas/Identity/Data/UIMVCContext.cs
+++ UI-MVC/Areas/Identity/Data/UIMVCContext.cs
@@ -1,27 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
-using UIMVC.Areas.Identity.Data;
 
-namespace UIMVC.Models
+namespace UIMVC.Areas.Identity.Data
 {
-    public class UIMVCContext : IdentityDbContext<UIMVCUser>
+    public class UimvcContext : IdentityDbContext<UimvcUser>
     {
-        public UIMVCContext(DbContextOptions<UIMVCContext> options)
+        public UimvcContext(DbContextOptions<UimvcContext> options)
             : base(options)
         {
         }
-
-        /*protected override void OnModelCreating(ModelBuilder builder)
-        {
-            base.OnModelCreating(builder);
-            // Customize the ASP.NET Identity model and override the defaults if needed.
-            // For example, you can rename the ASP.NET Identity table names and more.
-            // Add your customizations after calling base.OnModelCreating(builder);
-        }*/
     }
 }
