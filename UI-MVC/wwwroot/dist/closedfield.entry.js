e2566fa22cd25d390f99eb1472acdce446c20074
Niels Van Zandbergen <niels.vanzandbergen@student.kdg.be>
2019-05-18 20:23:47 +0200

    Closed Field Done

--- UI-MVC/wwwroot/dist/closedfield.entry.js
+++ UI-MVC/wwwroot/dist/closedfield.entry.js
@@ -93,83 +93,130 @@
 /*! no static exports found */
 /***/ (function(module, exports) {
 
-let closedFieldBody = document.querySelector(".addClosedField");
-let fieldStringsContainer = document.querySelector(".addFieldStringsContainer");
-let fieldButton = document.querySelector(".fieldStringButton");
-let containersToPutFieldStringsIn = document.querySelectorAll(".fieldStringContainer");
-let addedFieldStrings = 0;
-
 console.log("closedfield active!");
 
-function reassignIndex() {
+let addedFieldStrings = 0;
+
+function addDivRow(index){
+    let out = document.createElement("div");
+    out.classList.add("inputcontainerRow");
+    out.classList.add("row");
+    out.setAttribute("id", index);
+    
+    return out;
+}
+
+function addDivColumn(){
+    let out = document.createElement("div");
+    out.classList.add("inputcontainerColumn");
+    out.classList.add("col-6");
+    
+    return out;
+}
+
+function addInputPart(index){
+    let out = document.createElement("input");
+    out.setAttribute("type", "text");
+    out.setAttribute("class", "form-control");
+    out.classList.add("fieldStringsInput");
+    out.setAttribute("id", index);
+    out.setAttribute("name", "FieldStrings[" + index + "]");
+    
+    return out;
+}
+
+function addRemoveButton(index){
+    let out = document.createElement("a");
+    out.classList.add("btn");
+    out.classList.add("btn-danger");
+    out.classList.add("destroyFieldString");
+    out.innerHTML = "X";
+    out.setAttribute("id", index);
+    
+    return out;
+}
+
+function collectContainer(Parent){
+    let containers = document.querySelectorAll(".fieldStringContainer");
+    let out = undefined;
+
+    for(let i = 0; i < containers.length; i++){
+        console.log(containers[i].getAttribute("id"));
+        if(containers[i].getAttribute("id") === Parent){
+            out = containers[i];
+        }
+    }
+    
+    return out;
+}
+
+function redoIndexes(Parent){
+    let container = collectContainer(Parent);
     addedFieldStrings = 0;
 
-    let allInputs = document.querySelectorAll(".fieldStringsInput");
-    let allRemoveButtons = document.querySelectorAll(".destroyFieldString");
-    let allcontainers = document.querySelectorAll(".inputcontainer");
+    let allInputs =  container.querySelectorAll(".fieldStringsInput");
+    let allRemoveButtons = container.querySelectorAll(".destroyFieldString");
+    let allrows = container.querySelectorAll(".inputcontainerRow");
 
-    for (var i = 0; i < allInputs.length; i++) {
+    for (let i = 0; i < allInputs.length; i++) {
         addedFieldStrings = i;
 
         allInputs[i].setAttribute("name", "FieldStrings[" + addedFieldStrings + "]");
-        allcontainers[i].setAttribute("data-option", addedFieldStrings);
-        allRemoveButtons[i].setAttribute("data-option", addedFieldStrings);
+        allInputs[i].setAttribute("id", addedFieldStrings);
+        allrows[i].setAttribute("id", addedFieldStrings);
+        allRemoveButtons[i].setAttribute("id", addedFieldStrings);
 
     }
 
     addedFieldStrings = allInputs.length;
+    
 }
 
-function removeFieldString() {
-    console.log("Removing element: " + this.getAttribute("data-option"));
+function destroyClosedFieldPart(Parent, index){
+    console.log("Removing element: " + Parent + "." + index);
+    
+    let containerrows = document.querySelectorAll(".inputcontainerRow");
 
-    let index = this.getAttribute("data-option");
-    let allcontainers = document.querySelectorAll(".inputcontainer");
-
-    for (var i = 0; i < allcontainers.length; i++) {
-
-        if (index === allcontainers[i].getAttribute("data-option")) {
-            console.log("Attribute compare -> Index: " + index + " Allcontainers[" + i + "]: " + allcontainers[i].getAttribute("data-option").value);
-            allcontainers[i].remove();
+    for (let i = 0; i < containerrows.length; i++) {
+        if(containerrows[i].getAttribute("id") === index){
+            containerrows[i].remove();
         }
     }
-
-    reassignIndex();
+    
+    redoIndexes(Parent);
 }
 
+function addClosedFieldPart(Parent){
+    let container = collectContainer(Parent);
+    
+    let inputcontainer = addDivRow(addedFieldStrings);
+    let leftinputcolumn = addDivColumn();
+    let rightinputcolumn = addDivColumn();
+    let input = addInputPart(addedFieldStrings);
+    
+    let destroybutton = addRemoveButton(addedFieldStrings);
+    destroybutton.addEventListener("click",function(){
+        destroyClosedFieldPart(Parent, destroybutton.getAttribute("id"))
+    });
 
-function addFieldString(index) {
-    console.log("Adding Field String: " + addedFieldStrings);
-    let input = document.createElement("input");
-    input.setAttribute("type", "text");
-    input.setAttribute("class", "form-control");
-    input.classList.add("fieldStringsInput");
-    input.setAttribute("name", "FieldStrings[" + addedFieldStrings + "]");
+    leftinputcolumn.appendChild(input);
+    rightinputcolumn.appendChild(destroybutton);
+    inputcontainer.appendChild(leftinputcolumn);
+    inputcontainer.appendChild(rightinputcolumn);
+    container.appendChild(inputcontainer);
 
-    let inputcontainer = document.createElement("div");
-    inputcontainer.classList.add("inputcontainer");
-    inputcontainer.setAttribute("data-option",addedFieldStrings);
-
-    let removebutton = document.createElement("a");
-    removebutton.classList.add("btn");
-    removebutton.classList.add("btn-danger");
-    removebutton.classList.add("destroyFieldString");
-    removebutton.innerHTML = "X";
-    removebutton.setAttribute("data-option", addedFieldStrings);
-    removebutton.addEventListener("click", removeFieldString);
-
-    inputcontainer.appendChild(input);
-    inputcontainer.appendChild(removebutton);
-
-
-    containersToPutFieldStringsIn[index].appendChild(inputcontainer);
     addedFieldStrings++;
+    
+    console.log("New Part added for: " + Parent);
 }
 
 const buttons = document.querySelectorAll(".addFieldStringsButton");
-    
 for(let i = 0; i < buttons.length; i++){
-    buttons[i].addEventListener("click", addFieldString(i));
+    buttons[i].addEventListener("click", function(){
+        let id = this.getAttribute("id");
+        console.log("ParentIdea will be: " + id);
+        addClosedFieldPart(id);
+    });
 }
 
 /***/ })
