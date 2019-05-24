cf61957f1d748dd3bdeb823210b086edd92aba09
Sacha Buelens <sacha.buelens@student.kdg.be>
2019-05-24 22:43:22 +0200

    Docu

--- UI-MVC/Controllers/API/PlatformController.cs
+++ UI-MVC/Controllers/API/PlatformController.cs
@@ -1,6 +1,14 @@
 using BL;
 using Microsoft.AspNetCore.Mvc;
 
+
+//Controller for platforms
+//Author: Edwin Kai-Yin Tam
+
+//Edited by: David Matei, Edwin Kai-Yin Tam
+
+    //This is a small controller to get all platforms and send them to the android app, so users can pick a platform 
+
 namespace UIMVC.Controllers.API
 {
     [ApiController]
